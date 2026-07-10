76a2b41bd651aaec91e8993e3da265f01cd101c6
Sinalo Kebetwana <230104096@mycput.ac.za>
2026-07-10 17:36:11 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    FooterComponent
+  ],
   template: `
     <app-navbar></app-navbar>
+
     <main class="page-wrapper">
       <router-outlet></router-outlet>
     </main>
+
+    <app-footer></app-footer>
   `
 })
 export class AppComponent {
